d051cc0d853c4b19ce1b88300a8f79d29542b447
nazar017 <lykasnazik@gmail.com>
2024-12-10 19:06:49 +0200

    finish lesson 1

---
+++ lesson_1/hw_4/main.ts
@@ -0,0 +1,5 @@
+let firstName:string = prompt('What is your first name?');
+let lastName :string= prompt('What is your last name?');
+let yearsOld :string= prompt('How old are you?');
+
+console.log(`Hello my i'm ${firstName} ${lastName}. I'm a ${yearsOld} years old.`);
