2cece65952746c16a03c0ef6c5bb8bc4f320caf9
nazar017 <lykasnazik@gmail.com>
2024-12-10 16:32:17 +0200

    Initial commit

---
+++ lesson_1/hw_1/main.ts
@@ -0,0 +1,38 @@
+let hello: string = 'hello';
+console.log(hello);
+
+let owu: string = 'owu';
+console.log(owu);
+
+let com: string = 'com';
+console.log(com);
+
+let ua: string = 'ua';
+console.log(ua);
+
+let var1: number = 1;
+console.log(var1);
+
+let var2: number = 10;
+console.log(var2);
+
+let var3: number = -999;
+console.log(var3);
+
+let var4: number = 123;
+console.log(var4);
+
+let var5: number = 3.14;
+console.log(var5);
+
+let var6: number = 2.7;
+console.log(var6);
+
+let var7: number = 16;
+console.log(var7);
+
+let var8: boolean = true;
+console.log(var8);
+
+let var9: boolean = false;
+console.log(var9);
---
+++ lesson_1/hw_2/main.ts
@@ -0,0 +1,5 @@
+let firstName:string = 'Nazar'
+let middleName:string = 'Mykhailovych'
+let lastName:string = 'Natrobin'
+
+console.log(`${firstName} ${middleName} ${lastName}`);
